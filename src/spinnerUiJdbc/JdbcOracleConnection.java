1256e79d40d789c9283dddd1988496f8002e43d2
Sissodiya,Aditya <Aditya.Sissodiya@cerner.com>
2019-12-19 15:40:21 +0530

    Query Fetching

--- src/spinnerUiJdbc/JdbcOracleConnection.java
+++ src/spinnerUiJdbc/JdbcOracleConnection.java
@@ -4,50 +4,145 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 public class JdbcOracleConnection {
+	private static final String activityForMicrobiology = "696";
+	private static final String queryForInstrumentName = "SELECT instr_name FROM lab_instrument "
+			+ "WHERE service_resource_cd IN "
+			+ "(select SERVICE_RESOURCE_CD from SERVICE_RESOURCE sr where sr.ACTIVITY_TYPE_CD="
+			+activityForMicrobiology
+			+ ")";
+	private static final String queryForDispatchDownload = "SELECT sr.dispatch_download_ind FROM service_resource sr,"
+			+ "lab_instrument li WHERE li.instr_name='MICRO_VIDYA'"
+			+ "AND sr.service_resource_cd = li.service_resource_cd";
 	
+	private static final String queryForCollectedDownload = "SELECT sr.collected_download_ind FROM service_resource sr,"
+			+ "lab_instrument li WHERE li.instr_name='MICRO_VIDYA'"
+			+ "AND sr.service_resource_cd = li.service_resource_cd";
+	
+	private static final String queryForR_Mode = "SELECT sr.oper_mode FROM service_resource sr,"
+			+ "lab_instrument li WHERE li.instr_name='MICRO_VIDYA'"
+			+ "AND sr.service_resource_cd = li.service_resource_cd";
+	
+	
+	protected ResultSet rsInstName;
+	protected ResultSet rsDispDown;
+	protected ResultSet rsCollDown;
+	protected ResultSet rsR_Mode;
+	
+	int resultSetSize = 0;
+	int index = 0;
+	
+	String[] instrumetNameArray = new String [100];
+	String[] dispatchDownloadArray = new String [100];
+	String[] collectedDownloadArray = new String [100];
+	String[] rModeArray = new String [100]; 
+	String[] instrumetNameArrayFinal ;
 	
 	public void databaseAction() {
-		 Connection conn2 = null;
+		 
+		Connection conn = null;
+		Statement stmtInstName = null;
+		Statement stmtDispatchDownload = null;
+		Statement stmtCollectedDownload = null;
+		Statement stmtR_Mode = null;
 		 
 	        try {
 	            
-	            Class.forName("oracle.jdbc.OracleDriver");
-	 
-	            String dbURL2 = "jdbc:oracle:thin:@ipsurrounddb.ip.devcerner.net:1521:surd1";
+	            //Driver Registration
+				Class.forName("oracle.jdbc.OracleDriver");
+				
+				//Connection String
+	           final String dbURL2 = "jdbc:oracle:thin:@ipsurrounddb.ip.devcerner.net:1521:surd1";
+	           final String username = "v500";
+	           final String password = "v500";
+				
+	            //Establishing Connection
+	            conn = DriverManager.getConnection(dbURL2, username, password);
 	            
-	            String username = "v500";
-	            String password = "v500";
+	            if (conn != null) {
+	                System.out.println("Connected to Database Surround");
+	            }
+	            //Querying Database
+
+	             stmtInstName = conn.createStatement();
 	            
-	            conn2 = DriverManager.getConnection(dbURL2, username, password);
-	            
-	            
-	            if (conn2 != null) {
-	                System.out.println("Connected with connection #2");
+	            boolean statusForInstName = stmtInstName.execute(queryForInstrumentName);
+	            if(statusForInstName){
+	                //query is a select query.
+	                 rsInstName = stmtInstName.executeQuery(queryForInstrumentName);
+	                 
+	                while(rsInstName.next()){
+	                	resultSetSize++;
+	                	instrumetNameArray [index] = rsInstName.getString(1);
+	                	index++;	                	
+	                }
+	                instrumetNameArrayFinal = new String[resultSetSize];
+	                
+	                for(int i=0;i<resultSetSize;i++) {
+	                	 if(instrumetNameArray[i] != null) {
+	 	                	instrumetNameArrayFinal [i] = instrumetNameArray[i];
+	 	                }else {
+	 	                	instrumetNameArrayFinal [i] = "xxx";
+	 	                }
+	                }
+	               
+	                
+	                System.out.println("resultSize :"+ resultSetSize);
+	                
+	                rsInstName.close();
+	            } else {
+	                //query can be update or any query apart from select query
 	            }
 	            
-	            Statement stmt = conn2.createStatement();
-	            
-	            String query = "select SERVICE_RESOURCE_CD from SERVICE_RESOURCE sr where sr.activity_type_cd = 696";
-	            
-	            //select INSTR_NAME from SERVICE_RESOURCE sr where sr.activity_type_cd = 696
-	            //select INSTR_NAME from LAB_INSTRUMENT where UPDT_TASK = 1234703
-	            //select * from SERVICE_RESOURCE sr where sr.activity_type_cd = 696
-	            
-	            boolean status = stmt.execute(query);
-	            if(status){
+	            stmtDispatchDownload = conn.createStatement();
+	            boolean statusForDispDown = stmtDispatchDownload.execute(queryForDispatchDownload);
+	            if(statusForDispDown){
 	                //query is a select query.
-	                ResultSet rs = stmt.getResultSet();
-	                while(rs.next()){
-	                    System.out.println(rs.getString(1));
+	            	rsDispDown = stmtDispatchDownload.executeQuery(queryForDispatchDownload);
+	                 
+	                while(rsDispDown.next()){
+	                	
+	                	System.out.println("Dispatch Download : " + rsDispDown.getString(1));
+	    	                	
 	                }
-	                rs.close();
+	                
+	                rsDispDown.close();
 	            } else {
 	                //query can be update or any query apart from select query
 	            }
 	 
+	            stmtCollectedDownload = conn.createStatement();
+	            boolean statusForCollDown = stmtCollectedDownload.execute(queryForCollectedDownload);
+	            if(statusForCollDown){
+	                //query is a select query.
+	            	rsCollDown = stmtCollectedDownload.executeQuery(queryForCollectedDownload);
+	                 
+	                while(rsCollDown.next()){
+	                	System.out.println("Collected Download : " + rsCollDown.getString(1));     	
+	                }
+	                rsCollDown.close();
+	            } else {
+	                //query can be update or any query apart from select query
+	            }
+	            
+	            stmtR_Mode = conn.createStatement();
+	            boolean statusForR_Mode = stmtR_Mode.execute(queryForR_Mode);
+	            if(statusForR_Mode){
+	                //query is a select query.
+	            	rsR_Mode = stmtR_Mode.executeQuery(queryForR_Mode);
+	                 
+	                while(rsR_Mode.next()){
+	                	
+	                	System.out.println("R Mode : " + rsR_Mode.getString(1));
+	    	                	
+	                }
+	                
+	                rsR_Mode.close();
+	            } else {
+	                //query can be update or any query apart from select query
+	            }
+	            
 	 
 	        } catch (ClassNotFoundException ex) {
 	            ex.printStackTrace();
@@ -55,8 +150,8 @@ public class JdbcOracleConnection {
 	            ex.printStackTrace();
 	        } finally {
 	            try {
-	                if (conn2 != null && !conn2.isClosed()) {
-	                    conn2.close();
+	                if (conn != null && !conn.isClosed()) {
+	                    conn.close();
 	                }
 	            } catch (SQLException ex) {
 	                ex.printStackTrace();
@@ -67,5 +162,6 @@ public class JdbcOracleConnection {
     public static void main(String[] args) {
     	
        System.out.println("called JdbcOracleConnection main()");
-    }
+       
+		}
 }
--- src/spinnerUiJdbc/JdbcSelectionScreen.java
+++ src/spinnerUiJdbc/JdbcSelectionScreen.java
@@ -4,17 +4,27 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Menu;
+
+import java.util.Arrays;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.widgets.Button;
 
-public class JdbcSelectionScreen extends JdbcOracleConnection {
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.List;
 
+public class JdbcSelectionScreen extends JdbcOracleConnection{
+	
+	
 	protected Shell shell;
-
+	 JdbcOracleConnection jdbcOC = new JdbcOracleConnection();
+	 
+	int i=0;
+	
 	/**
 	 * Launch the application.
 	 * @param args
@@ -23,6 +33,7 @@ public class JdbcSelectionScreen extends JdbcOracleConnection {
 		try {
 			JdbcSelectionScreen window = new JdbcSelectionScreen();
 			window.open();
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -51,12 +62,13 @@ public class JdbcSelectionScreen extends JdbcOracleConnection {
 		
 		shell = new Shell();
 		shell.setSize(450, 300);
-		shell.setText("microbiologyInstruments");
+		shell.setText("microbiologyInstrumentsStats");
+		shell.setLayout(new FormLayout());
 		
 		Menu menuBar = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(menuBar);
 		
-		 // Create the File item's dropdown menu
+		 // Create the File item's drop down menu
 	    Menu fileMenu = new Menu(menuBar);
 
 	    // Create all the items in the bar menu
@@ -64,7 +76,7 @@ public class JdbcSelectionScreen extends JdbcOracleConnection {
 	    fileItem.setText("File");
 	    fileItem.setMenu(fileMenu);
 
-	    // Create all the items in the File dropdown menu
+	    // Create all the items in the File drop down menu
 	    MenuItem newItem = new MenuItem(fileMenu, SWT.NONE);
 	    newItem.setText("New");
 	    MenuItem openItem = new MenuItem(fileMenu, SWT.NONE);
@@ -75,35 +87,80 @@ public class JdbcSelectionScreen extends JdbcOracleConnection {
 	    saveAsItem.setText("Save As...");
 
 	    shell.setMenuBar(menuBar);
+	    
 	    shell.open();
-	    /*
-	     * while (!shell.isDisposed()) {
-	      if (!display.readAndDispatch()) {
-	        display.sleep();
-	      }
-	    }
-	     * */
+	    
+	    List list = new List(shell, SWT.DROP_DOWN);
 		
-		ListViewer listViewer = new ListViewer(shell, SWT.BORDER | SWT.V_SCROLL);
-		List list = listViewer.getList();
-		list.setBounds(10, 61, 408, 143);
-		
-		
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.setBounds(299, 10, 119, 35);
-		btnNewButton.setText("executeQuery");
+	    Button btnNewButton = new Button(shell, SWT.NONE);
+
 		btnNewButton.addListener(SWT.Selection, (Listener) new Listener() {
 		      public void handleEvent(Event e) {
 		        switch (e.type) {
 		        case SWT.Selection:
 		          System.out.println("Query In Execution");
 		          
-		          JdbcOracleConnection jdbcOC = new JdbcOracleConnection();
-					jdbcOC.databaseAction();
+		        //Calling databaseAction() through a JdbcOracleConnection object |jdbcOC|
 		          
-					break;
+		          jdbcOC.databaseAction();
+				 
+		          
+					System.out.println(jdbcOC.rsInstName.toString());
+					int instArrayLength = jdbcOC.resultSetSize;
+					
+					for (int loopIndex = 0; loopIndex < instArrayLength; loopIndex++) {
+					      list.add(jdbcOC.instrumetNameArrayFinal[loopIndex]);
+					    }
+
+		          break;
 		        }
 		      }
 		    });
+		
+		FormData fd_btnNewButton = new FormData();
+		fd_btnNewButton.right = new FormAttachment(100, -10);
+		fd_btnNewButton.top = new FormAttachment(0);
+		btnNewButton.setLayoutData(fd_btnNewButton);
+		btnNewButton.setText("executeQuery");
+		
+		FormData fd_list = new FormData();
+		fd_list.bottom = new FormAttachment(btnNewButton, 0, SWT.BOTTOM);
+		fd_list.right = new FormAttachment(btnNewButton, -6);
+		fd_list.top = new FormAttachment(0);
+		fd_list.left = new FormAttachment(0, 10);
+		list.setLayoutData(fd_list);
+		
+		 // Print selection
+        list.addListener(SWT.Selection, new Listener() {
+            @Override
+            public void handleEvent(Event arg0) {
+                if(list.getSelectionCount() > 0)
+                    System.out.println(Arrays.toString(list.getSelection()));
+            }
+        });
+		
+		Button btnCheckButtonDispatchDownload = new Button(shell, SWT.CHECK);
+		FormData fd_btnCheckButton = new FormData();
+		btnCheckButtonDispatchDownload.setLayoutData(fd_btnCheckButton);
+		btnCheckButtonDispatchDownload.setText("DispatchDownload");
+		
+		Button btnCheckButtonCollectedDownload = new Button(shell, SWT.CHECK);
+		fd_btnCheckButton.bottom = new FormAttachment(btnCheckButtonCollectedDownload, -6);
+		fd_btnCheckButton.left = new FormAttachment(btnCheckButtonCollectedDownload, 0, SWT.LEFT);
+		FormData fd_btnCheckButton_1 = new FormData();
+		fd_btnCheckButton_1.left = new FormAttachment(0, 108);
+		btnCheckButtonCollectedDownload.setLayoutData(fd_btnCheckButton_1);
+		btnCheckButtonCollectedDownload.setText("CollectedDownload");
+		
+		Button btnCheckButtonR_mode= new Button(shell, SWT.CHECK);
+		fd_btnCheckButton_1.bottom = new FormAttachment(btnCheckButtonR_mode, -6);
+		FormData fd_btnCheckButton_2 = new FormData();
+		fd_btnCheckButton_2.bottom = new FormAttachment(100, -10);
+		fd_btnCheckButton_2.left = new FormAttachment(0, 108);
+		btnCheckButtonR_mode.setLayoutData(fd_btnCheckButton_2);
+		btnCheckButtonR_mode.setText("R_mode");
+		
 	}
 }
+	
+
